1e19b51847234e05d969156a6db3d59b5746228d
bono <bono@podotree.com>
2019-06-08 00:52:58 +0900

    네이버 & 구글 웹마스터 등록

--- src/components/list.js
+++ src/components/list.js
@@ -15,8 +15,18 @@ export default function Posts({ data, location, isTagPage, pageContext }) {
     <Layout location={location}>
       <Helmet
         htmlAttributes={{ lang: 'ko' }}
-        meta={[{ name: 'description', content: siteDescription }]}
         title={siteTitle}
+        meta={[
+          { name: 'description', content: siteDescription },
+          {
+            name: 'naver-site-verification',
+            content: '23eddf7267bb527337927baff11fc26157637f30',
+          },
+          {
+            name: 'google-site-verification',
+            content: 'R3ZeY1PmaJUY9j_cgNABjesJFgHWVCJpb4TsdfXntMA',
+          },
+        ]}
       />
       {isTagPage && (
         <div css={{ margin: '20px auto 60px', textAlign: 'center' }}>
